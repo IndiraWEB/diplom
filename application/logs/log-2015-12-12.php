08b5750f04b2aa1a129ee54f7e9d3d6c5b9f3aa8
indriaWEB <indira@web.com.kz>
2015-12-15 13:40:03 +0600

    portal

---
+++ application/logs/log-2015-12-12.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-12-12 19:38:18 --> 404 Page Not Found: Img/action_bakalavr.jpg
+ERROR - 2015-12-12 19:38:18 --> 404 Page Not Found: Img/action_magistr.jpg
+ERROR - 2015-12-12 19:38:19 --> 404 Page Not Found: Img/action_doctor.jpg
+ERROR - 2015-12-12 19:38:19 --> 404 Page Not Found: Assets/action_js
+ERROR - 2015-12-12 19:38:19 --> 404 Page Not Found: Img/action_news.jpg
+ERROR - 2015-12-12 19:38:19 --> 404 Page Not Found: Assets/action_js
+ERROR - 2015-12-12 19:38:20 --> 404 Page Not Found: Img/action_news.jpg
+ERROR - 2015-12-12 19:38:20 --> 404 Page Not Found: Img/action_magistr.jpg
+ERROR - 2015-12-12 19:38:20 --> 404 Page Not Found: Img/action_bakalavr.jpg
+ERROR - 2015-12-12 19:38:20 --> 404 Page Not Found: Img/action_doctor.jpg
+ERROR - 2015-12-12 19:38:20 --> 404 Page Not Found: Faviconico/action_index
+ERROR - 2015-12-12 19:38:20 --> 404 Page Not Found: Faviconico/action_index
